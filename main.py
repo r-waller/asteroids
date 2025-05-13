e54b28433c21ad44e3c11eca2a28ed5b3bf58b7d
r-waller <nunya@bizness.com>
2025-05-12 19:41:12 -0500

    Added player movement and groups
    Added functionality for the player to move when pressing WASD and made
    groups for update and draw methods.

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ def main():
     print(f"Screen height: { SCREEN_HEIGHT }")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2) , (SCREEN_HEIGHT / 2))
-    dt = 0 
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 return
-        screen.fill("black")
-        player.draw(screen)
-        player = Player((SCREEN_WIDTH / 2) , (SCREEN_HEIGHT / 2))
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
+        updatable.update(dt)
+        screen.fill("black")
+        for x in drawable:
+            x.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
         main()
